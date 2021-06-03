6f6be4ad22eb54a1d21d2752fb05bc0e5c12fa4d
choimory <choimory@naver.com>
2021-06-03 12:41:58 +0900

    Initial commit

---
+++ src/main/java/com/practice/helloresponseentity/HelloResponseEntityApplication.java
@@ -0,0 +1,13 @@
+package com.practice.helloresponseentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloResponseEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloResponseEntityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/helloresponseentity/HelloResponseEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.helloresponseentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloResponseEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
