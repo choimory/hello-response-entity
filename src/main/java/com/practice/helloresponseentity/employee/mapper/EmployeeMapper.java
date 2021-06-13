7acbc7ac74448c41af5e701a80ca3560c129e7db
choimory <choimory@naver.com>
2021-06-13 13:43:47 +0900

    mapstruct

--- src/main/java/com/practice/helloresponseentity/employee/mapper/EmployeeMapper.java
+++ src/main/java/com/practice/helloresponseentity/employee/mapper/EmployeeMapper.java
@@ -1,4 +1,12 @@
 package com.practice.helloresponseentity.employee.mapper;
 
+import com.practice.helloresponseentity.employee.dto.response.EmployeeResponseDto;
+import com.practice.helloresponseentity.employee.entity.Employee;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+@Component
 public interface EmployeeMapper {
+    EmployeeResponseDto toDto(Employee entity);
 }
