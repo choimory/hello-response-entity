94a617f777e8fe07e98b1c54fcb9a43e123f2af5
choimory <choimory@naver.com>
2021-07-24 17:49:58 +0900

    CommonResponse객체명을 Data에서 Dto로 변경

--- src/main/java/com/practice/helloresponseentity/employee/EmployeeController.java
+++ src/main/java/com/practice/helloresponseentity/employee/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.practice.helloresponseentity.employee;
 
-import com.practice.helloresponseentity.common.response.code.CommonResponseCode;
-import com.practice.helloresponseentity.common.response.util.CommonResponseData;
+import com.practice.helloresponseentity.common.response.dto.CommonResponseDto;
 import com.practice.helloresponseentity.employee.dto.request.EmployeeRequestDto;
 import com.practice.helloresponseentity.employee.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
@@ -19,8 +18,8 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     @GetMapping("/{employeeId}")
-    public ResponseEntity<CommonResponseData<?>> getEmployee(@PathVariable final Long employeeId){
-        return new ResponseEntity<>(CommonResponseData.builder()
+    public ResponseEntity<CommonResponseDto<?>> getEmployee(@PathVariable final Long employeeId){
+        return new ResponseEntity<>(CommonResponseDto.builder()
                                                         //@Builder.default 및 초기화를 OK로 해놨으므로 OK일시엔 생략가능
                                                         //.statusCode(CommonResponseCode.OK.getCode())
                                                         //.statusMessage(CommonResponseCode.OK.getMessage())
