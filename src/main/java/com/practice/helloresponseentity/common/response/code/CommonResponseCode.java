fa32f14841bd9a2e58a0eeb939ed99f794011031
choimory <choimory@naver.com>
2021-09-20 18:23:49 +0900

    공통코드 HTTP상태코드에 맞추어 변경

--- src/main/java/com/practice/helloresponseentity/common/response/code/CommonResponseCode.java
+++ src/main/java/com/practice/helloresponseentity/common/response/code/CommonResponseCode.java
@@ -6,11 +6,22 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CommonResponseCode {
-    INVALID(100, "잘못된 요청입니다")
-    , EMPTY(101, "데이터가 없습니다")
-    , OK(200, "정상 처리되었습니다")
-    , NOT_FOUND(404, "경로를 찾을 수 없습니다")
-    , INTERNAL_SERVER_ERROR(500, "서버 오류입니다");
+    CONTINUE(100, "")
+
+    , OK(200, "정상")
+    , CREATED(201, "등록되었습니다")
+    , NO_CONTENT(204, "값이 없습니다")
+
+    , BAD_REQUEST(400, "잘못된 요청입니다")
+    , UNAUTHORIZED(401, "인증이 필요합니다")
+    , FORBIDDEN(403, "권한이 없습니다")
+    , NOT_FOUND(404, "경로를 찾지 못함")
+    , NOT_ALLOWED(405, "해당 메소드로는 요청할 수 없습니다")
+    , REQUEST_TIMEOUT(408, "요청시간 초과")
+    , TOO_MANY_REQUESTS(429, "시간당 요청제한 초과")
+
+    , INTERNAL_SERVER_ERROR(500, "서버 오류")
+    , BAD_GATEWAY (502, "");
 
     private final Integer code;
     private final String message;
